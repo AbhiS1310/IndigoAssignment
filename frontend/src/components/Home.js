a54234e26cb3780ca6752f19642daf28e5518b79
Abhishek <chaudhary.abhi627@gmail.com>
2024-07-29 23:16:16 +0530

    Initial commit

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Layout/Footer'
+import Flights from './Flights/Flights'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Flights/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default Home
